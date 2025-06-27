a4787a881902cebfc9657e4aa061e5d066eaa2f7
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-06-27 16:31:22 -0300

    Realice todo lo solicitado en el punto 2

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Text.Json;
 using Usuarios;
 
 class Program
@@ -7,6 +9,39 @@ class Program
     static async Task Main(string[] args)
     {
         using HttpClient client = new HttpClient();
+
+        HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+        response.EnsureSuccessStatusCode();
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+        List<Usuario> ListUsuarios = JsonSerializer.Deserialize<List<Usuario>>(responseBody);
+
+        /*
+        foreach (var usu in ListUsuarios)
+        {
+            Console.WriteLine($"Nombre: {usu.name}\nCorreo electronico: {usu.email}\nDireccion: {usu.address.street}");
+            Console.WriteLine("-------------------------");
+        }
+        */
+        List<string> usuariosDatos = new List<string>
+        {
+            "Nombre del usuario ;Correo electronico ;Direccion "
+        };
+
+        foreach (var usu in ListUsuarios)
+        {
+            if (usu.id <= 5)
+            {
+                Console.WriteLine($"ID: {usu.id}\nNombre: {usu.name}\nCorreo electronico: {usu.email}\nDireccion: {usu.address.street}");
+                Console.WriteLine("-------------------------");
+                string agregar = $"{usu.name}; {usu.email}; {usu.address.street}";
+                usuariosDatos.Add(agregar);
+            }
+        }
         
+        string jsonString = JsonSerializer.Serialize(usuariosDatos, new JsonSerializerOptions { WriteIndented = true });
+        string ruta = Path.Combine(Directory.GetCurrentDirectory(), "Usuarios.json");
+        File.WriteAllText(ruta, jsonString);
+        Console.WriteLine("'Usuarios.json' creado y cargado con exito.");
     }
 }
--- Usuarios/obj/Debug/net8.0/Usuarios.AssemblyInfo.cs
+++ Usuarios/obj/Debug/net8.0/Usuarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e814405857672e2ed57d416ab02ef283fb50226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484b3e3747b87249875d84946cd4e9a523c10e76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
